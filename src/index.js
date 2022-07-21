74a881b16b322591f79aaeb81cababacee731a04
Rustam <krr12@mail.ru>
2022-07-21 09:27:00 +0500

    zapR

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ZapravkaValue from './zapravkaValue';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ZapravkaValue />
+  </React.StrictMode>
+);
+
