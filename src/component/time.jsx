19f849c6f4a181938125783f61a1d10325e88657
Rustam <krr12@mail.ru>
2022-07-24 12:50:52 +0500

    time

--- src/component/time.jsx
+++ src/component/time.jsx
@@ -1,18 +1,11 @@
-import React, { useState, useEffect,useRef} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 export default function Time() {
-   const [date, setDate] = useState(new Date()); 
-
-   useEffect(() => {
-      const timerId = setInterval(() => setDate(new Date()),1000)      
-      return () => {
-         clearInterval(timerId)
-      };
-   });
-
+   const [date, setDate] = useState(new Date());
+   useEffect(() => setDate(new Date()))
    return (
-      <div>         
-         <h6> Информация обновлена : <br />{date.toLocaleString()}</h6>             
+      <div>
+         <h6> Информация обновлена : <br />{date.toLocaleString()}</h6>
       </div>
    )
 }
